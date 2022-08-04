f8cf1ca5d00f08c7d7ed59e89b1463c2444e3782
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-08-04 11:38:08 -0600

    Added answer tag

--- Desmos/DesmosTags.py
+++ Desmos/DesmosTags.py
@@ -4,3 +4,4 @@ module = Module()
 
 module.tag('desmos_simple_exponentiation', desc = 'Enables simple exponentiation commands')
 
+module.tag('desmos_answer', desc = 'Enables previous answer command')p
